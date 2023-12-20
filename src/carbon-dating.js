d204f1d9d2b9e3a21a9db58302dd1174c18c0b87
Lesia Soloviova <solovyova.lesya@gmail.com>
2023-12-20 22:15:17 +0200

    feat: solve task 1

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,44 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  // Convert sampleActivity to a number
+  const activity = parseFloat(sampleActivity);
+
+  // Check if the input is a valid number and within the appropriate range
+  if (
+    isNaN(activity) ||
+    typeof sampleActivity !== "string" ||
+    activity <= 0 ||
+    activity > MODERN_ACTIVITY
+  ) {
+    return false;
+  }
+
+  // Calculate the age using the radioactive decay formula
+  const age = Math.ceil(
+    Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD)
+  );
+
+  // Check if the calculated age is a valid positive number
+  if (isNaN(age) || age <= 0) {
+    return false;
+  }
+
+  return age;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
