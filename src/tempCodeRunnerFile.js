9971cb40793410faa45684817be42dd74696cb08
Lesia Soloviova <solovyova.lesya@gmail.com>
2023-12-22 18:14:04 +0200

    feat: solve tasks 1-13

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,9 +1,24 @@
-function getEmailDomain(email) {
-  const splittedEmailArray = email.split("@");
-  console.log(splittedEmailArray);
-  let domain = splittedEmailArray[splittedEmailArray.length-1];
-return domain;
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    console.log("matrix[i]", matrix[i]);
+    for (let j = 0; j < matrix[i].length; j++) {
+console.log("j",j);
+console.log("i",i);
+      if (i === 0 || matrix[i - 1][j] !== 0) {
+        console.log("matrix[i][j]", matrix[i][j]);
+console.log("matrix[i][j]", matrix[i][j]);
+        sum += matrix[i][j];
+      }
+    }
+  }
 
+  return sum;
 }
-
-console.log(getEmailDomain("prettyandsimple@example.com"));
+console.log(
+  getMatrixElementsSum([
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3],
+  ])
+);
